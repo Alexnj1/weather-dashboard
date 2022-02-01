8e61bd7623b665870844e176c9cb36442be17009
Alex Noble-James <alexnoble143.an@gmail.com>
2022-02-01 02:13:52 -0500

    added search history for the cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,35 +5,45 @@ var searchTerm = document.querySelector('input')
 var citySaveArea = document.querySelector('.city-save')
 var weatherDisplay = document.querySelector('.weather-display')
 var futureCardDisplay = document.querySelector('.future-cards')
-var cities = []
+var resetButton =  document.querySelector('.reset')
+var clearButton = document.querySelector('.clear-save')
+// var cityButton = document.querySelector ('.saved-city-button')
+var cities = JSON.parse(localStorage.getItem('cities')) || []
 
 
 function getLatLong(city) {
     var lat
     var long
 
-    fetch ('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=imperial&appid=4db674b8f49814db2b9ea15729fac8ea').then(function(response) {
-    if (response.ok) {
-        response.json()
-        .then(function(data){
-            storeCity()
-            lat = data.coord.lat.toString()
-            long = data.coord.lon.toString()
-
-            getWeatherData(lat, long)
-
-            
-            cities.push({
-            name: city
-            })
-            storeCity(cities)
-
-        })
+    if (!city) {
+        alert ('You must enter a city!')
     } else {
-        alert ('Sorry, this city is not found.')
-    }
+        fetch ('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=imperial&appid=4db674b8f49814db2b9ea15729fac8ea').then(function(response) {
+            if (response.ok) {
+                response.json()
+                .then(function(data){
+                    // storeCity()
+                    lat = data.coord.lat.toString()
+                    long = data.coord.lon.toString()
+
+                    getWeatherData(lat, long)
+
+                    
+                    cities.unshift({
+                    name: city
+                    })
+                    if (cities.length > 5) {
+                        cities.length = 5
+                    }
+                    storeCity(cities)
+
+                })
+            } else {
+                alert ('Sorry, this city is not found.')
+            }
     
-  })
+        })
+    }
 }
 
 function getWeatherData (lat, long) {
@@ -49,10 +59,16 @@ function getWeatherData (lat, long) {
 }
 
 function displayCurrentWeatherData (data) {
-// 
+    weatherDisplay.textContent = ''
     var date = moment().format('MMM Do YYYY')
     var city = document.createElement('h3')
-    city.textContent = (searchTerm.value + ' (' + date + ')').toUpperCase()
+
+    // if (!searchTerm.value) {
+        // city.textContent = (cityButton.textContent + ' (' + date + ')').toUpperCase()
+    // } else {
+        city.textContent = (searchTerm.value + ' (' + date + ')').toUpperCase()
+
+    // }
 
     var temp = document.createElement('p')
     temp.textContent = ('Temp: ' + data.current.temp + '°F')
@@ -76,7 +92,7 @@ function displayCurrentWeatherData (data) {
     var uv = document.createElement('p')
     uv.textContent = ('UV Index: ')
 
-    console.log(city, temp, wind, humidity, uvNumber)
+    // //(city, temp, wind, humidity, uvNumber)
     uv.appendChild(uvNumber)
     weatherDisplay.appendChild(city)
     weatherDisplay.appendChild(temp)
@@ -84,23 +100,21 @@ function displayCurrentWeatherData (data) {
     weatherDisplay.appendChild(humidity)
     weatherDisplay.appendChild(uv)
 
-    searchTerm.value = ''
+    // searchTerm.value = ''
 }
 
 function displayFutureWeatherData (data) {
-    console.log(data)
-    // 
+    // //(data)
     date = moment()
-    console.log(date)
+    // //(date)
     futureCardDisplay.textContent = ''
-    // console.log(test)
-
     for (i=0; i<=4; i++) {
        var container = document.createElement('div')
        container.classList = 'future-card'
+
        var day = document.createElement('h4')
        day.textContent = date.add(1, 'days').format('MM/DD/YYYY')
-// 
+
        var temp = document.createElement('p')
        temp.textContent = ('Temp: ' + data.daily[i].temp.day + '°F')
 
@@ -124,42 +138,73 @@ function storeCity (cities) {
 }
 
 function loadCity () {
-    var cities = JSON.parse(localStorage.getItem('cities'))
-    console.log (cities)
+    // // (cities)
 
     if (cities == null) {
         clickListener()
     } else {
-        for (i=0; i<cities.length ; i++) {
+        // // (cities.length -= 1)
+        // ctrl z from here, you were testing if you could manually keep the display at the end by defining i
+        // try to put the loop back and define i like this
+        // i=cities.length -1
             
-        
+        for (i=0; i<cities.length; i++) { 
             var cityButton = document.createElement('button')
-            
+            cityButton.className = 'saved-city-button'
+
             cityButton.textContent = cities[i].name
         
             citySaveArea.appendChild(cityButton)
-        
-            var savedCity = cities[i].name
 
-            searchTerm.value = cities[i].name
+            
                 
-            cityButton.addEventListener('click', function() {
-                getLatLong(savedCity)
+            cityButton.addEventListener('click', function(event) {
+            var savedCity = event.target.textContent
+            searchTerm.value = event.target.textContent
+            //(event.target.textContent)
+            getLatLong(savedCity)
             })
-        } 
-        clickListener()
+         
+        }
+        
+        
+        // (cityButton.textContent)
+        
+        searchTerm.value = ''
+        clickListener(cityButton)
+
+
     }
     
 }
 
-function clickListener () {
-    searchButton.addEventListener('click', function getCity() {
+
+
+function clickListener (cityButton) {
+    
+    searchButton.addEventListener('click', function() {
+    searchTerm.disabled = true
     weatherDisplay.textContent = ''
     var city = searchTerm.value
-    console.log (city)
     getLatLong(city)
   })
+
+//     cityButton.addEventListener('click', function() {
+//     var savedCity = cityButton.textContent
+//     // (savedCity)
+//     getLatLong(savedCity)
+//   })
 }
+
+resetButton.addEventListener ('click', function() {
+    document.location.reload(true)
+})
+
+clearButton.addEventListener ('click', function() {
+    localStorage.removeItem('cities')
+    document.location.reload(true)
+    // storeCity()
+})
 
 
 loadCity()
